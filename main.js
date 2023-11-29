d53ad78898bb9af26002a81b387b8f63e61e0a16
bextiyar <azimovbextiyar1989@gmail.com>
2023-11-29 20:50:24 +0400

    a

---
+++ main.js
@@ -0,0 +1,40 @@
+// const content =document.querySelector(".content");
+
+// const btn =document.getElementById("click");
+// btn.addEventListener('click',function(){
+// content.classList.toggle('active')
+// })
+
+
+
+
+                ///////////////   29.11.2023 /////////////////
+
+function Employee(fullName, age, position) {
+    this.fullName =fullName;
+    this.age =age;
+    this.position= position;
+}
+
+let emp1 = new Employee("John Doe", 25, "Fronted");
+
+
+
+
+
+// class Car  {
+//     constructor(model, year, color) {
+//         this.model =model;
+//         this.year =year;
+//         this.color =color;
+//     }
+// }
+// let car =new Car("Hyundai",2020,"blue");
+// console.log(car);
+
+
+const obj = {
+    fullName:"test",
+    age:34,
+    position:"Fronted"
+}
